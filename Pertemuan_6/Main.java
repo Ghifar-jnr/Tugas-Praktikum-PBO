8765c701c9938612f7ff1707f83daec04c111f4b
Ghifar-jnr <ghifar4523051@univpancasila.ac.id>
2024-10-10 16:23:25 +0700

    Add files via upload

---
+++ Pertemuan_6/Main.java
@@ -0,0 +1,28 @@
+package Pertemuan_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Jagoan jakaSembung = new Jagoan("Jaka Sembung");
+        Jubah jubahPutih = new Jubah("Jubah Putih", 7, 50);
+        Senjata golok = new Senjata("Golok", 25);
+        jakaSembung.setArmor(jubahPutih);
+        jakaSembung.setWeapon(golok);
+
+        Jagoan pitung = new Jagoan("Si Pitung");
+        Jubah jubahHitam = new Jubah("Jubah Hitam", 8, 52);
+        Senjata toya = new Senjata("Toya", 24);
+        pitung.setArmor(jubahHitam);
+        pitung.setWeapon(toya);
+
+        jakaSembung.display();
+        pitung.display();
+
+        jakaSembung.displayjs();
+        pitung.displayjs();
+        System.out.println();
+
+        jakaSembung.attack(pitung);
+        pitung.attack(jakaSembung);
+        pitung.attack(jakaSembung);
+    }
+}
